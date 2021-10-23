7be6982b889adab482a6ecfff6f28a2592b6cd1f
Guilherme Galarça Dutra <udutra@gmail.com>
2021-10-23 19:34:26 -0300

    Configuração da Cinemachine.

--- Assets/Game/Scripts/Player/PlayerController.cs
+++ Assets/Game/Scripts/Player/PlayerController.cs
@@ -14,6 +14,15 @@ public class PlayerController : MonoBehaviour {
 
     public Sprite crouchedSprite, idleSprite;
 
+    [Header("Camera")]
+    public Transform cameraTarget;
+    [Range(0f, 5.0f)]
+    public float cameraTargetOffsetX;
+    [Range(0.5f, 50.0f)]
+    public float  cameraTargetflipSpeed;
+    [Range(0f, 5.0f)]
+    public float characterSpeedInfluence;
+
     private void Start() {
         playerMovement = GetComponent<CharacterMovement2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
@@ -57,5 +66,16 @@ public class PlayerController : MonoBehaviour {
             //TODO: Remover quando adicionar animação
             spriteRenderer.sprite = idleSprite;
         }
+    }
+
+    private void FixedUpdate() {
+        //Ajuste da camera
+        bool isFacingRight = spriteRenderer.flipX == false;
+        float targetOffsetX = isFacingRight ? cameraTargetOffsetX : -cameraTargetOffsetX;
+        float currentOffsetX = Mathf.Lerp(cameraTarget.localPosition.x, targetOffsetX, Time.fixedDeltaTime * cameraTargetflipSpeed);
+
+        currentOffsetX += playerMovement.CurrentVelocity.x * Time.fixedDeltaTime * characterSpeedInfluence;
+
+        cameraTarget.localPosition = new Vector3(currentOffsetX, cameraTarget.localPosition.y, cameraTarget.localPosition.z);
     }
 }
